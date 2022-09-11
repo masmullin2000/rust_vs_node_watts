202b316129d15596ed97244b9ca3aa8628c50a80
Michael Mullin <masmullin@gmail.com>
2022-09-10 22:29:39 -0400

    optimized rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
+#![allow(dead_code)]
+#![allow(clippy::unnecessary_unwrap)]
+
+use core::slice;
+use std::fs::read_dir;
 use std::path::PathBuf;
+use std::{cell::RefCell, fmt::Write};
 
 use anyhow::Result;
-use axum::response::{IntoResponse, Html};
-use async_recursion::async_recursion;
+use axum::http::header;
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+};
+use serde::Serialize;
+use sqlx::{postgres::PgRow, PgPool, Pool, Postgres, Row};
 
-use std::fs::read_dir;
+const FILE: &str = include_str!("../manpage");
 
 fn walk(file_list: &mut Vec<String>, root: PathBuf) -> Result<()> {
     let dir = read_dir(root)?;
@@ -13,10 +24,12 @@ fn walk(file_list: &mut Vec<String>, root: PathBuf) -> Result<()> {
         let entry = entry?;
         let meta = entry.metadata()?;
         if meta.is_dir() {
-            _ = walk(file_list, entry.path())?;
+            walk(file_list, entry.path())?;
         } else if meta.is_file() {
-            let s = entry.path().to_string_lossy().to_string();
-            file_list.push(s);
+            if let Some(s) = entry.path().to_str() {
+                let s = String::from(s);
+                file_list.push(s);
+            }
         }
     }
 
@@ -27,15 +40,84 @@ pub async fn file_list() -> impl IntoResponse {
     let mut resp_h = String::from("<ul>");
 
     let mut file_list = Vec::new();
-    _ = walk(&mut file_list, PathBuf::from("/code/mainline/"));
+    if walk(&mut file_list, PathBuf::from("/my_tmp/mainline/")).is_ok() {
+        file_list.sort_unstable();
 
-    file_list.sort_unstable();
-
-    for file in file_list {
-        resp_h.push_str(&format!("<li>{}</li>\n", file));
+        for file in file_list {
+            _ = writeln!(resp_h, "<li>{}</li>", file);
+        }
+        resp_h.push_str("</ul>");
     }
-    resp_h.push_str("</ul>");
 
     Html(resp_h)
 }
+
+pub async fn file() -> impl IntoResponse {
+    Html(FILE)
+}
+
+type sstring = smallstr::SmallString<[u8; 16]>;
+#[derive(Serialize)]
+struct User {
+    uid: i32,
+    first_name: sstring,
+    last_name: sstring,
+}
+
+async fn get_sql(pool: Pool<Postgres>) -> Result<Vec<PgRow>> {
+    let db_res = sqlx::query("SELECT * from users")
+        .bind("uid")
+        .bind("first_name")
+        .bind("last_name")
+        .fetch_all(&pool)
+        .await?;
+    Ok(db_res)
+}
+
+fn get_users<'a>(sql_rows: Vec<PgRow>) -> &'a [User] {
+    thread_local! {
+        static USERS: RefCell<Vec<User>> = RefCell::new(Vec::with_capacity(1000));
+    }
+
+    USERS.with(|u| {
+        let users = &mut *u.borrow_mut();
+        users.clear();
+
+        sql_rows.into_iter().for_each(|row| {
+            let uid: i32 = row.get(0);
+            let fname: &str = row.get(1);
+            let lname: &str = row.get(2);
+            users.push(User {
+                uid,
+                first_name: sstring::from(fname),
+                last_name: sstring::from(lname),
+            })
+        });
+        //users.shrink_to(1000);
+
+        let ptr = users.as_ptr();
+        unsafe { slice::from_raw_parts(ptr, users.len()) }
+    })
+}
+
+fn get_resp(sql: Vec<PgRow>) -> Vec<u8> {
+    let mut resp = Vec::with_capacity(65_535);
+    let users = get_users(sql);
     
+    let writer = std::io::BufWriter::new(&mut resp);
+    serde_json::to_writer(writer, users).expect("blah");
+
+    resp
+}
+
+pub async fn users(State(pool): State<PgPool>) -> impl IntoResponse {
+    let users = if let Ok(sql) = get_sql(pool).await {
+        let resp = get_resp(sql);
+        let r = unsafe { String::from_utf8_unchecked(resp) };
+        r
+    } else {
+        "".to_string()
+    };
+
+    ([(header::CONTENT_TYPE, "application/json")], users) 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 use std::net::SocketAddr;
 
-use axum::{Router, Server, routing::get};
-use ::lib::file_list;
+use ::lib::{file, file_list, users};
+use axum::{routing::get, Router, Server};
 
 mod lib;
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(file_list));
+use mimalloc::MiMalloc;
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
-    let server = Server::bind(&addr);
+const PORT: u16 = 8000;
 
-    server.serve(app.into_make_service())
+async fn run(port: u16) {
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .connect("postgress://mm:.@localhost/list_of_users")
         .await
         .unwrap();
+
+    let app = Router::with_state(pool)
+        .route("/file_list", get(file_list))
+        .route("/file", get(file))
+        .route("/users", get(users));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let server = Server::bind(&addr);
+
+    server.serve(app.into_make_service()).await.unwrap();
+}
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let port = if args.len() >= 2 {
+        args[1].parse::<u16>().unwrap_or(PORT)
+    } else {
+        PORT
+    };
+
+    let threads = if args.len() >= 3 {
+        args[2].parse::<usize>().unwrap_or_else(|_| num_cpus::get())
+    } else {
+        num_cpus::get()
+    };
+
+    println!("Running on port {port} with {threads} threads");
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(threads)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async { run(port).await });
 }
