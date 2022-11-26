db8b7edb1feed1e4a7419150df01cbca273c73b5
Michael Mullin <masmullin@gmail.com>
2022-11-25 20:41:11 -0500

    update for axum 0.6

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -11,26 +11,22 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 const PORT: u16 = 8000;
 
-async fn run(port: u16) {
-    /*let pool = sqlx::postgres::PgPoolOptions::new()*/
-    /*.connect("postgress://mm:.@localhost/list_of_users")*/
-    /*.await*/
-    /*.unwrap();*/
-    let manager = bb8_postgres::PostgresConnectionManager::new_from_stringlike(
-        "host=localhost user=mm password=. dbname=list_of_users",
-        NoTls,
-    )
-    .unwrap();
+async fn run(port: u16, user: &str, pword: &str, dbname: &str) {
+    let conn_param = format!("host=localhost user={user} password={pword} dbname={dbname}");
+
+    let manager =
+        bb8_postgres::PostgresConnectionManager::new_from_stringlike(&conn_param, NoTls).unwrap();
 
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
 
-    let app = Router::with_state(pool)
+    let app = Router::new()
         .route("/file_list", get(file_list))
         .route("/file", get(file))
         .route("/users_json", get(users_json))
         .route("/users_json_agg", get(users_json_agg))
         .route("/users_json_manual", get(users_json_manual))
-        .route("/users_html", get(users_html));
+        .route("/users_html", get(users_html))
+        .with_state(pool);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let server = Server::bind(&addr);
@@ -61,5 +57,5 @@ fn main() {
         .build()
         .unwrap();
 
-    rt.block_on(async { run(port).await });
+    rt.block_on(async { run(port, "postgres", "password", "list_of_users").await });
 }
